74c567bebdc7576812765d8c4019c6b4ec74e8c4
kkowa <k.kowalewska89@wp.pl>
2022-08-14 22:27:37 +0200

    set check in and check out

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+
+    @Test
+    public void searchHotel() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        //Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@class='select2-match']")).click();
+        driver.findElement(By.name("checkin")).sendKeys("17/09/2022");
+        driver.findElement(By.name("checkout")).sendKeys("22/09/2022");
+
+
+    }
+}
